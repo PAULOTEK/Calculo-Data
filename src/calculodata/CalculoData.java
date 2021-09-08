5b61f83c4e1fbefe322d9870255a161699c7e8a1
PAULOTEK <pauloalexandre820@gmail.com>
2021-09-08 20:37:03 -0300

    commit

---
+++ src/calculodata/CalculoData.java
@@ -0,0 +1,21 @@
+
+package calculodata;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author noemi.oliveira
+ */
+public class CalculoData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new CalculoDataGUI().setVisible(true);
+    }
+    
+}
